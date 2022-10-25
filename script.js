3d330276e2a6c248fb6e85e108a0b547c08c4586
forsmsmail2019 <forsmsmail2019@gmail.com>
2022-10-25 09:25:34 -0400

    s

--- script.js
+++ script.js
@@ -129,3 +129,20 @@ function newsheet(e){
     }
    
 }
+
+
+  function multiplyNumeric(obj) {
+    for (let key in obj) {
+      if (typeof obj[key] == 'number') {
+        obj[key] *= 2;
+      }
+    }
+  }
+
+let user = {
+  a : 200,
+  w : 300,
+  as : "hello",
+ }
+multiplyNumeric(user)
+console.log(user)
